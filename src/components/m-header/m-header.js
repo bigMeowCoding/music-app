c40b78bbab73dc336a4303675eeea3bf21b92a8c
zhouyijun <zhouyijun@ruijie.com.cn>
2020-04-11 00:43:03 +0800

    init

---
+++ src/components/m-header/m-header.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import './m-header.scss'
+export function MHeader() {
+    return (
+        <div className="m-header">
+            <div className="icon"></div>
+            <h1 className="text">Chicken Music</h1>
+        </div>
+    )
+
+}
