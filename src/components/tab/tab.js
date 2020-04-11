97d11d056e8ba1b3578b7b075230066a3147192a
zhouyijun <zhouyijun@ruijie.com.cn>
2020-04-11 14:51:57 +0800

    导航栏功能开发完毕

--- src/components/tab/tab.js
+++ src/components/tab/tab.js
@@ -1,29 +1,52 @@
 import * as React from "react";
+
 import {Link} from 'react-router-dom';
 
+import './tab.scss';
+import {useState} from "react";
+
 export function Tab() {
+    let menuConfig = [
+        {
+            label: '推荐',
+            path: '/recommend',
+            type: 'recommend'
+        },
+        {
+            label: '歌手',
+            path: '/singer',
+            type: 'singer'
+        },
+        {
+            label: '排行',
+            path: '/rank',
+            type: 'rank'
+        },
+        {
+            label: '搜索',
+            path: '/search',
+            type: 'search'
+        }
+    ];
+    const [selectedMenu, setSelectedMenu] = useState(menuConfig[0])
+
+    function selectMenu(menuItem) {
+        setSelectedMenu(menuItem)
+    }
+
     return (
         <div className="tab">
-            <li className="tab-item">
-                <Link to='/recommend'>
-                    <span className="tab-link">推荐</span>
-                </Link>
-            </li>
-            <li className="tab-item">
-                <Link to="/singer">
-                    <span className="tab-link">歌手</span>
-                </Link>
-            </li>
-            <li className="tab-item">
-                <Link to="/rank">
-                    <span className="tab-link">排行</span>
-                </Link>
-            </li>
-            <li className="tab-item">
-                <Link to="/search">
-                    <span className="tab-link">搜索</span>
-                </Link>
-            </li>
+            {
+                menuConfig.map((config) => {
+                    return <li key={config.type}
+                               className={`tab-item${selectedMenu && selectedMenu.type === config.type ? ' selected' : ''}`}
+                               onClick={(e) => selectMenu(config)}>
+                        <Link to={config.path}>
+                            <span className="tab-link">{config.label}</span>
+                        </Link>
+                    </li>
+                })
+            }
         </div>
     )
 }
