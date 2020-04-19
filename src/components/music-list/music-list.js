bd6d02aaa56fb5ef5cae2b3f1f799bdca236614e
zhouyijun <zhouyijun@ruijie.com.cn>
2020-04-19 21:17:02 +0800

    滚动layer上移功能

--- src/components/music-list/music-list.js
+++ src/components/music-list/music-list.js
@@ -1,29 +1,65 @@
-import React, {useEffect, useMemo, useRef} from "react";
+import React, {useEffect, useMemo, useRef, useState} from "react";
 import './music-list.scss';
 import {useHistory} from "react-router-dom";
 import {Scroll} from "../../base/scroll/scroll";
 import {SongList} from "../../base/song-list/song-list";
 
+const RESERVED_HEIGHT = 40
+
 export function MusicList(props) {
     let {title, bgImage, songs} = props;
+    let [scrollY, setScrollY] = useState(0);
+    let [minTranslateY, setMinTranslateY] = useState(0);
     const bgImageRef = useRef(),
         songListRef = useRef(),
+        playBtnRef = useRef(),
+        layerRef = useRef(),
         history = useHistory(), listenScroll = true, probeType = 3;
     useEffect(() => {
         if (bgImageRef.current) {
+            const bgImageHeight = bgImageRef.current.clientHeight;
             bgImageRef.current.style = `background-image:url(${bgImage})`;
+            setMinTranslateY(-1 * bgImageHeight + RESERVED_HEIGHT);
             if (songListRef.current) {
-                songListRef.current.style.top = `${bgImageRef.current.clientHeight}px`;
+                songListRef.current.style.top = `${bgImageHeight}px`;
             }
         }
     }, [bgImage])
-    // useEffect(() => {
-    //     this.imageHeight = this.$refs.bgImage.clientHeight
-    //     // this.minTransalteY = -this.imageHeight + RESERVED_HEIGHT
-    // }, [])
+    useEffect(() => {
+        let translateY = Math.max(minTranslateY, scrollY), zIndex = 0;
+        const layerEle = layerRef.current, bgEle = bgImageRef.current, playBtnEle = playBtnRef.current;
+        if (layerEle) {
+            layerEle.style.transform = `translate3d(0,${translateY}px,0)`
+        }
+        if (scrollY > 0) {
+            zIndex = 10;
+        }
+        if (bgEle) {
+            if (scrollY < minTranslateY) {
+                zIndex = 10;
+                bgEle.style.height = `${RESERVED_HEIGHT}px`;
+                bgEle.style.paddingTop = 0;
+                if (playBtnEle) {
+                    playBtnEle.style.display = 'none';
+                }
+            } else {
+                bgEle.style.height = 0;
+                bgEle.style.paddingTop = '70%';
+                if (playBtnEle) {
+                    playBtnEle.style.display = '';
+                }
+            }
+            bgEle.style.zIndex = zIndex;
+        }
+
+    }, [scrollY])
 
     function back() {
         history.goBack()
+    }
+
+    function getScrollPos(pos) {
+        setScrollY(pos.y);
     }
 
     return (
@@ -36,15 +72,18 @@ export function MusicList(props) {
             </h1>
             <div className="bg-image" ref={bgImageRef}>
                 <div className="play-wrapper">
-                    <div className="play">
+                    <div className="play" ref={playBtnRef}>
                         <i className="icon-play"/>
                         <span className="text">随机播放全部</span>
                     </div>
                 </div>
                 <div className="filter"/>
             </div>
+            <div className="bg-layer" ref={layerRef}/>
             <div className="list" ref={songListRef}>
-                <Scroll data={songs} listenScroll={listenScroll} probeType={probeType}>
+                <Scroll data={songs}
+                        getPos={getScrollPos}
+                        listenScroll={listenScroll} probeType={probeType}>
                     <div className="song-list-wrapper">
                         <SongList songs={songs}/>
                     </div>
